80442343874e206b320b53c2b2e844736bd7e1ee
JenceSon <98326682+JenceSon@users.noreply.github.com>
2023-12-03 15:53:38 +0700

    Update BTL_answer.sql

--- database setup file/BTL_answer.sql
+++ database setup file/BTL_answer.sql
@@ -1,8 +1,9 @@
-use BTL
+use BTL_db_official
 go
 --1/ create trigger
-create trigger update_no_productname 
-on Product_name
+--drop trigger update_no_product
+create trigger update_no_product 
+on Product
 after insert, delete
 as
 begin
@@ -30,7 +31,7 @@ begin
 	while @@FETCH_STATUS = 0
 	begin
 		update Shop
-		set no_productname = no_productname + @exceed
+		set no_product = no_product  + @exceed
 		where shop_id = @shop_id
 		fetch next from cur into @shop_id
 	end
@@ -40,7 +41,7 @@ begin
 end;
 go
 create trigger update_min_max_totalremain
-on Version
+on Variant
 after insert, delete,update
 as
 begin
@@ -49,34 +50,34 @@ begin
 	--------------------
 	if exists (select 0 from inserted) --insert or update
 	begin
-		declare cur Cursor for select distinct productname_id from inserted
+		declare cur Cursor for select distinct product_id from inserted
 	end
 	else
 	begin
-		declare cur Cursor for select distinct productname_id from deleted
+		declare cur Cursor for select distinct product_id from deleted
 	end
 
-	declare @productname_id varchar(9);
-	declare @min decimal(10,1);
-	declare @max decimal(10,1);
-	declare @totalremain int;
+	declare @product_id varchar(9);
+	declare @min decimal(10,1) = 0;
+	declare @max decimal(10,1) = 0;
+	declare @totalremain int = 0;
 	open cur
-	fetch from cur into @productname_id
+	fetch from cur into @product_id
 	--------------------
 	while @@FETCH_STATUS = 0
 	begin
 		set @min = (
 			select min(price) 
-			from version 
-			where productname_id = @productname_id)
+			from Variant 
+			where product_id = @product_id)
 		set @max = (
 			select max(price)
-			from Version
-			where productname_id = @productname_id)
+			from Variant
+			where product_id = @product_id)
 		set @totalremain = (
 			select sum(remaining_amount)
-			from Version
-			where productname_id = @productname_id)
+			from Variant
+			where product_id = @product_id)
 
 
 		if @min is null set @min = 0
@@ -84,19 +85,20 @@ begin
 		if @totalremain is null set @totalremain = 0
 		
 		
-		update Product_name
+		update Product
 		set minimum_price = @min ,
 			maximum_price = @max,
 			total_remaining = @totalremain
-		where productname_id = @productname_id
-		fetch from cur into @productname_id
+		where product_id = @product_id
+		fetch from cur into @product_id
 	end
 	close cur
 	deallocate cur
 	--------------------
 end
 go
-create trigger update_no_productname_of_order
+--drop trigger update_no_productname_of_order
+create trigger update_no_product_of_order
 on Is_contained
 after insert, delete
 as
@@ -113,30 +115,30 @@ begin
 		declare cur cursor for (select distinct order_id from deleted);
 	end
 	declare @order_id varchar(14);
-	declare @no_productname int;
-	declare @list_productname table(productname_id varchar(9));
+	declare @no_product int;
+	declare @list_product table(product_id varchar(9));
 	open cur
 	fetch from cur into @order_id
 	--------------------
 	while @@FETCH_STATUS = 0
 	begin
-		insert into @list_productname
-		select distinct p.productname_id
-		from Is_contained i, Product p
-		where i.order_id = @order_id and p.product_id = i.product_id;
+		insert into @list_product
+		select distinct p.product_id
+		from Is_contained i, Product_instance p
+		where i.order_id = @order_id and p.instance_id = i.instance_id;
 		
-		set @no_productname = (select count(*) from @list_productname)
+		set @no_product = (select count(*) from @list_product)
 		update [Order]
-		set no_productname = @no_productname
+		set no_product = @no_product
 		where order_id = @order_id
 		fetch from cur into @order_id
-		delete @list_productname
+		delete @list_product
 	end
 	close cur
 	deallocate cur
 end
 go
-create trigger update_no_productname_of_cart
+create trigger update_no_product_of_cart
 on [Add]
 after insert,delete
 as
@@ -154,59 +156,110 @@ begin
 	end
 	declare @user_id varchar(9);
 	declare @number		int;
-	declare @list_productname table (productname_id varchar(9));
 	open cur;
 	fetch from cur into @user_id, @number;
 	---------------------
 	while @@FETCH_STATUS = 0
 	begin
-		insert into @list_productname
-		select distinct productname_id
-		from Product p, [Add] a
-		where p.product_id = a.product_id and a.number = @number and a.user_id=@user_id;
-
 		update Shopping_cart
-		set no_productname = (select count(*) from @list_productname)
+		set no_product = (select count(*) from [Add] where user_id = @user_id and number = @number )
 		where user_id = @user_id and number = @number;
 
 		fetch from cur into @user_id, @number;
-		delete @list_productname
 	end
 	close cur
 	deallocate cur
 
 end
 go
+--drop trigger check_n_update_quantity_Voucher
+create trigger check_n_update_quantity_Voucher
+on Applies
+after Insert
+as
+begin
+	declare cur Cursor for (select voucher_id from inserted);
+	declare @vch varchar(9)
+	open cur
+	fetch from cur into @vch;
+
+	while @@FETCH_STATUS = 0
+	begin
+		update Voucher
+		set quantity = quantity - 1
+		where voucher_id = @vch
+
+		fetch from cur into @vch
+	end
+	close cur
+	deallocate cur
+end
+go
+--drop trigger check_available
+create trigger check_available
+on Applies
+for Insert
+as
+begin
+	declare cur Cursor for (select * from inserted)
+	declare @ord varchar(14)
+	declare @vch varchar(9)
+
+	open cur
+	fetch from cur into @ord, @vch
+
+	while @@FETCH_STATUS = 0
+	begin
+		if not exists (
+			select * from Can_apply
+			where voucher_id = @vch and
+			(category_id = 'CATffffff' or category_id in (
+				select distinct p.category_id from Is_contained i, Product_instance pin, Product p
+				where i.order_id = @ord and i.instance_id = pin.instance_id and pin.product_id = p.product_id)) and
+			(shop_id = 'SIDffffff' or shop_id in (
+				select distinct p.shop_id from Is_contained i, Product_instance pin, Product p
+				where i.order_id = @ord and i.instance_id = pin.instance_id and pin.product_id = p.product_id))
+		)
+		begin
+			rollback
+		end
+		fetch from cur into @ord, @vch
+	end
+	close cur
+	deallocate cur
+end
+go
 ---2 insert, delete, update
---drop procedure insert_productname
-create procedure insert_productname 
-@productname_id varchar(9), --mendatory
+--drop procedure insert_product
+create procedure insert_product 
+@product_id varchar(9), --mendatory
 @des varchar(1000) = null, --optional
 @name varchar(50),	--mendatory
 @total_remaining int, --mendatory
 @price	decimal(10,1), --mendatory
 @cat varchar(9) = null, --optional
 @sid varchar(9), --mendatory
+@img varchar(1000) = null,
 @result varchar(1000) = '' Output --default = ''
 as
 begin
-	if exists (select productname_id from Product_name where productname_id = @productname_id)
-		set @result = @result + 'Product name has already existed_'
+	if exists (select product_id from Product where product_id = @product_id)
+		set @result = @result + 'Product ID has already existed_'
 	if not exists (select shop_id from Shop where shop_id = @sid)
-		set @result = @result + 'Invalid Shop_'
+		set @result = @result + 'Invalid Shop ID_'
 	if @cat is not null and not exists (select category_id from Category where category_id = @cat)
-		set @result = @result + 'Invalid Catgory_'
+		set @result = @result + 'Invalid Catgory ID_'
 	if @total_remaining < 0
 		set @result = @result + 'Total remaining can not be a negative number_'
 	if @price < 0 
 		set @result = @result + 'Price can not be a negative number_'
-	if @productname_id not like 'PNI%' or LEN(@productname_id) != 9
-		set @result = @result + 'Invalid Productname id format_'
+	if @product_id not like 'PNI%' or LEN(@product_id) != 9
+		set @result = @result + 'Invalid product id format_'
 	if @result = ''
 	begin
 		if @des is not null
-			insert into Product_name values (
-				@productname_id,
+			insert into Product values (
+				@product_id,
 				@des,
 				@name,
 				@total_remaining,
@@ -214,10 +267,11 @@ begin
 				@price,
 				@price,
 				@cat,
-				@sid)
+				@sid,
+				@img)
 		else 
-			insert into Product_name values (
-				@productname_id,
+			insert into Product values (
+				@product_id,
 				default,
 				@name,
 				@total_remaining,
@@ -225,51 +279,56 @@ begin
 				@price,
 				@price,
 				@cat,
-				@sid)
+				@sid,
+				@img)
 		set @result = 'Successfully adding'
 	end
 end
 go
---drop procedure delete_productname
-create procedure delete_productname
-@productname_id varchar(9),
+--drop procedure delete_product
+create procedure delete_product
+@product_id varchar(9),
 @result varchar(100) = '' output
 as
 begin
-	if not exists (select * from Product_name where productname_id = @productname_id)
+	if not exists (select * from Product where product_id = @product_id)
 		set @result = @result + 'Invalid product name_'
 	if @result = ''
 	begin
-		--delete belong and version fisrt
-		delete Belong_to where productname_id = @productname_id
-		delete Version where productname_id = @productname_id
+		--delete belong and variant fisrt
+		delete Belong_to where product_id = @product_id
+		delete Variant where product_id = @product_id
 
-		--not exist in add and is contained => delete
-		delete Product
-		where productname_id = @productname_id and
-		not exists (select * from [Add] a where a.product_id = product_id) and
-		not exists (select * from Is_contained i where i.product_id = product_id);
+		--not exist in is contained (mean is in order) => delete
+		delete Product_instance
+		where product_id = @product_id and
+		not exists (select * from Is_contained i where i.instance_id = instance_id);
 
-		--exist in add or is contained => set to null productname_id pniffffff)
-		update Product
-		set productname_id = 'PNIffffff'
-		where productname_id = @productname_id;
+		--exist in is contained => ref to null product_id pniffffff)
+		update Product_instance
+		set product_id = 'PIDffffff'
+		where product_id = @product_id;
 
 		--update review
 		update Review
-		set productname_id = 'PNIffffff'
-		where productname_id = @productname_id;
+		set product_id = 'PIDffffff'
+		where product_id = @product_id;
 
-		--delete product_name
-		delete Product_name where productname_id = @productname_id
+		--update Add
+		update [Add]
+		set product_id = 'PIDffffff'
+		where product_id = @product_id
+
+		--delete Product
+		delete Product where product_id = @product_id
 		set @result = 'Successfully deleting'
 	end
 end
 go
---drop procedure update_productname
-create procedure update_productname --when update, get all information from edit panel
-@pni_current varchar(9),
-@pni_new	varchar(9),
+--drop procedure update_product
+create procedure update_product --when update, get all information from edit panel
+@pid_current varchar(9),
+@pid_new	varchar(9),
 @des varchar(1000), 
 @name varchar(50),	
 @total_remaining int,
@@ -277,69 +336,70 @@ create procedure update_productname --when update, get all information from edit
 @min_price	decimal(10,1),
 @max_price	decimal(10,1),
 @cat varchar(9), 
-@sid varchar(9), 
+@sid varchar(9),
+@img varchar(1000),
 @result varchar(1000) = '' Output --default = ''
 as
 begin
-	declare @min_price_cur decimal(10,1) = (select minimum_price from Product_name where productname_id = @pni_current)
-	declare @max_price_cur decimal(10,1) = (select maximum_price from Product_name where productname_id = @pni_current)
+	declare @min_price_cur decimal(10,1) = (select minimum_price from Product where product_id = @pid_current)
+	declare @max_price_cur decimal(10,1) = (select maximum_price from Product where product_id = @pid_current)
 	
-	if @pni_current != @pni_new and exists(select * from Product_name where productname_id=@pni_new)
-		set @result = @result + 'Product name has already existed_'
-	if @pni_current != @pni_new and (@pni_new not like 'PNI%' or len(@pni_new) != 9)
-		set @result = @result + 'Invalid Productname id format_'
+	if @pid_current != @pid_new and exists(select * from Product where product_id=@pid_new)
+		set @result = @result + 'Product ID has already existed_'
+	if @pid_current != @pid_new and (@pid_new not like 'PNI%' or len(@pid_new) != 9)
+		set @result = @result + 'Invalid product id format_'
 	if @total_remaining < 0
 		set @result = @result + 'Total remaining can not be a negative number_'
 	if @cat is not null and not exists (select * from Category where category_id = @cat)
 		set @result = @result + 'Invalid category_'
 	if not exists (select * from Shop where shop_id = @sid)
 		set @result = @result + 'Invalid shop_'
-	if exists (select * from Version where productname_id = @pni_current) -- has version => can't modify total remaining or price
+	if exists (select * from Variant where product_id = @pid_current) -- has Variant => can't modify total remaining or price
 	begin
 		if @max_price_cur != @max_price or @min_price_cur != @min_price
-			set @result = @result + 'This product name has 1 or more versions,so the price cannot be modified here_'
-		if @total_remaining != (select total_remaining from Product_name where productname_id = @pni_current)
-			set @result = @result + 'This product name has 1 or more versions, so total remaining cannot be modified here_'
+			set @result = @result + 'This product name has 1 or more Variants,so the price cannot be modified here_'
+		if @total_remaining != (select total_remaining from Product where product_id = @pid_current)
+			set @result = @result + 'This product name has 1 or more Variants, so total remaining cannot be modified here_'
 	end
-	if not exists (select * from Version where productname_id = @pni_current) -- no version => max = min and >= 0
+	if not exists (select * from Variant where product_id = @pid_current) -- no Variant => max = min and >= 0
 	begin
 		if @min_price != @max_price
-			set @result = @result + 'There are not any versions in this product name, so the minimum price and maximum price must have the same value_'
+			set @result = @result + 'There are not any Variants in this product, so the minimum price and maximum price must have the same value_'
 		if @min_price <0 or @max_price < 0
 			set @result = @result + 'The price can not be a negative number_'
 	end
 
 	if @result = ''
 	begin
-		/*if @pni_current != @pni_new -- new pni => need to update product, belong, version and review
+		/*if @pid_current != @pid_new -- new pni => need to update product, belong, Variant and review
 		begin
 			-- drop constraint => update => add constrain
 			alter table Product drop constraint FK_product_pni
 			update Product
-			set productname_id = @pni_new
-			where productname_id = @pni_current;
+			set product_id = @pid_new
+			where product_id = @pid_current;
 
-			alter table Version drop constraint FK_version_pni
-			update Version --update cascade, no need to update belong
-			set productname_id = @pni_new
-			where productname_id = @pni_current;
+			alter table Variant drop constraint FK_Variant_pni
+			update Variant --update cascade, no need to update belong
+			set product_id = @pid_new
+			where product_id = @pid_current;
 
 			alter table Reiview drop constraint FK_review_pni
 			update Review
-			set productname_id = @pni_new
-			where productname_id = @pni_current;
+			set product_id = @pid_new
+			where product_id = @pid_current;
 
-			--update product_name
-			update Product_name
-			set productname_id = @pni_new
-			where productname_id = @pni_current;
+			--update Product
+			update Product
+			set product_id = @pid_new
+			where product_id = @pid_current;
 
-			alter table Product add constraint FK_product_pni foreign key (productname_id) references Product_name(productname_id)
-			alter table Version add constraint FK_version_pni foreign key (productname_id) references Product_name(productname_id)
-			alter table Review add constraint FK_review_pni foreign key (productname_id) references Product_name(productname_id)
+			alter table Product add constraint FK_product_pni foreign key (product_id) references Product(product_id)
+			alter table Variant add constraint FK_Variant_pni foreign key (product_id) references Product(product_id)
+			alter table Review add constraint FK_review_pni foreign key (product_id) references Product(product_id)
 		end*/
 
-		update Product_name
+		update Product
 		set [description] = @des,
 		[name] = @name,
 		total_remaining = @total_remaining,
@@ -348,16 +408,17 @@ begin
 		maximum_price = @max_price,
 		category_id = @cat,
 		shop_id = @sid,
-		productname_id = @pni_new --update cascade, no need to update on belong, version, product, review
-		where productname_id = @pni_current;
+		product_id = @pid_new, --update cascade, no need to update on belong, Variant, product, review, add
+		img = @img
+		where product_id = @pid_current;
 
 		set @result = 'Successfully updating'
 	end
 end
 go
 ---3 procedure
--- drop procedure filter_productname
-create procedure filter_productname
+-- drop procedure filter_product
+create procedure filter_product
 @total_remaining_min_filter int = -1,
 @total_remaining_max_filter int = -1,
 @no_sales_min_filter int = -1,
@@ -371,12 +432,13 @@ begin
 	select 
 	s.[name],
 	c.category_name,
-	p.productname_id, 
+	p.product_id, 
 	p.[name], p.[description], p.no_sales,
 	p.total_remaining, 
 	p.minimum_price,
-	p.maximum_price
-	from Product_name p, Shop s, Category c
+	p.maximum_price,
+	p.img
+	from Product p, Shop s, Category c
 	where
 		(@total_remaining_min_filter = -1 or p.total_remaining >= @total_remaining_min_filter) and
 		(@total_remaining_min_filter = -1 or p.total_remaining <= @total_remaining_max_filter) and
@@ -392,27 +454,27 @@ end
 go
 create procedure best_selling_store
 @no_sales_min int,
-@no_productname_min int
+@no_product_min int
 as
 begin
-	select s.shop_id, s.[name], count(*) as no_most_selling_pni
-	from Shop s, Product_name p
-	where s.shop_id = p.productname_id and p.no_sales >= @no_sales_min
+	select s.shop_id, s.[name], count(*) as no_most_selling_product
+	from Shop s, Product p
+	where s.shop_id = p.shop_id and p.no_sales >= @no_sales_min
 	group by s.shop_id, s.[name]
-	having count(*) >= @no_productname_min
-	order by no_most_selling_pni desc
+	having count(*) >= @no_product_min
+	order by no_most_selling_product desc
 end
 go
 ---4
 /*select * from Shop
-select * from Product_name
+select * from Product
 select * from Is_contained
 select * from Product
 select * from [Order]*/
 
 /*select sum(Prod.current_price)
-from Shop as Sh, Product_name as Prod_name, [Order] as Ord, Is_contained as Cont, Product as Prod
+from Shop as Sh, Product as Prod_name, [Order] as Ord, Is_contained as Cont, Product as Prod
 where Sh.shop_id = Prod_name.shop_id
-	and Prod_name.productname_id = Prod.productname_id
+	and Prod_name.product_id = Prod.product_id
 	and Prod.product_id = Cont.product_id
 	and Ord.order_id = Cont.order_id*/
